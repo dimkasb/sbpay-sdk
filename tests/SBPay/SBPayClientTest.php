981b139a044fb56fb5ec1d510b2f505314bd4a7e
dimka <dimkaben@notando.is>
2023-06-19 17:42:54 +0300

    SBPay SDK: init

---
+++ tests/SBPay/SBPayClientTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SBPay;
+
+use PHPUnit\Framework\TestCase;
+
+class SBPayClientTest extends TestCase
+{
+
+    public function testGetToken()
+    {
+
+    }
+
+    public function testSignedPut()
+    {
+
+    }
+
+    public function testSignedPost()
+    {
+
+    }
+
+    public function testSignedDelete()
+    {
+
+    }
+
+    public function testSubscriptions()
+    {
+
+    }
+
+    public function testPayments()
+    {
+
+    }
+
+    public function testSetHttpClient()
+    {
+
+    }
+
+    public function testGetSecret()
+    {
+
+    }
+
+    public function testRequest()
+    {
+
+    }
+
+    public function testSignedGet()
+    {
+
+    }
+
+    public function testGetMerchant()
+    {
+
+    }
+}
